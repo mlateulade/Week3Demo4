c017bf776bd4d46b89623ac879fe96e1b999d79f
mlateulade <77686194+mlateulade@users.noreply.github.com>
2021-06-27 12:36:35 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,27 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+  //for loop
+
+    for (int counter=1; counter<=3; counter++)
+    {
+        Console.WriteLine("Hello");
+    }
+    
+
+    for (int counter=10; counter >0; counter--)
+
+  {
+    Console.WriteLine(counter);
+
+  }
+  
+
+
+
+
+
+  }
+}
